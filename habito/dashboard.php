c9215d5ca88fc08cc132a75f9b75f00b71fdf2d5
GamerDoop7 <145089422+GyaraDeez@users.noreply.github.com>
2024-02-21 21:33:44 +0700

    Add files via upload

---
+++ habito/dashboard.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+.hi img {
+  float: right;
+  width: 50%;
+  height: 25%;
+  background: #555;
+}
+
+table {
+  border-style:solid;
+  border-width:3px;
+}
+
+table th {
+  margin-left:10px;
+  border-style:solid;
+  border-width:3px;
+}
+td{
+  border-style:solid;
+  border-width:2px;
+  border-color:black;
+}
+.red{
+  background-color:red;
+  color:white;
+}
+.yellow{
+  background-color:yellow;
+  color:black;
+}
+.green{
+  background-color:green;
+  color:white;
+}
+.blue{
+  background-color:#0042FF;
+  color:white;
+}</style>
+</head>
+<body>
+<div class="hi">
+  <img src="https://cdn1-production-images-kly.akamaized.net/57tuMloeRwUcXPGqSEpKDvNaPVc=/1x472:1080x1080/1200x675/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/4470522/original/076140900_1687020462-Manchester_City_-_Ilustrasi_Manchester_City_copy.jpg" alt="Manchester City Badge">
+  <h1>Welcome, (user)</h1>
+  <h3>ID: (id)</h3>
+ </div>
+
+ <div id="task">
+ <table>
+  <tr>
+    <th>Color</th>
+    <th>Task</th>
+    <th>Due Date</th>
+    <th>Done?</th>
+  </tr>
+  <tr>
+    <td class=red>Red</td>
+    <td class=red>____</td>
+    <td class=red>____</td>
+    <td class=red>x</td>
+  </tr>
+  <tr>
+    <td class=yellow>Yellow</td>
+    <td class=yellow>____</td>
+    <td class=yellow>____</td>
+    <td class=yellow>x</td>
+  </tr>
+  <tr>
+    <td class=green>Green</td>
+    <td class=green>____</td>
+    <td class=green>____</td>
+    <td class=green>x</td>
+  </tr>
+  <tr>
+    <td class=blue>Blue</td>
+    <td class=blue>____</td>
+    <td class=blue>____</td>
+    <td class=blue>x</td>
+  </tr>
+</table>
+ </div>
+</body>
+</html>
